4f41fd8d952abfe8a1bc0fc7aae838b350ce1269
GiovannaNO <giovanna10nunes@gmail.com>
2022-03-21 23:56:57 +0900

    Project's Initial Commit

---
+++ www/components/acao.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    document.querySelector("#alerta1").addEventListener("click", function(){
+    });
+    document.querySelector("#alerta2").addEventListener("click", function(){
+    });
+    document.querySelector("#beep").addEventListener("click", function(){
+    });
+    document.querySelector("#avibrar").addEventListener("click", function(){
+    });
+}
