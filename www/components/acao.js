5aabd23566decb72ccc9fa778ac55e36ebad0e6c
GiovannaNO <giovanna10nunes@gmail.com>
2022-03-29 00:13:05 +0900

    Commit final \(aula\).

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,10 +1,38 @@
 window.onload = function(){
     document.querySelector("#alerta1").addEventListener("click", function(){
+        function retorno(){
+
+        }
+        navigator.notification.alert("Você foi alertado?", retorno, "ALERTA", "Fui");
     });
+
     document.querySelector("#alerta2").addEventListener("click", function(){
+        function retorno(buttonIndex){
+            if(buttonIndex == 1){
+                navigator.app.exitApp();
+            }else{
+                return false;
+            }
+        }
+
+        navigator.notification.confirm(
+            "Deseja fechar o aplicativo?",
+            retorno,
+            "Fechar App",
+            ['Sim', 'Nâo']
+        );
+
     });
+
     document.querySelector("#beep").addEventListener("click", function(){
+
+        navigator.notification.beep(2);
+
     });
-    document.querySelector("#avibrar").addEventListener("click", function(){
+
+    document.querySelector("#vibrar").addEventListener("click", function(){
+
+        navigator.vibrate(3000);
+
     });
 }
